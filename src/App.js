60b584a1120273c87ce4f0a592fce897e40203cf
ivanchimenti <ivanchimenti@gmail.com>
2021-11-17 11:51:24 -0300

    Desafio Clase 3 II

--- src/App.js
+++ src/App.js
@@ -1,54 +1,43 @@
-import logo from './logo.svg';
+import React, { Fragment } from "react";
 import './App.css';
 
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
 
+/* 
+SI QUEREMOS DEVOLVER MAS DE UN ELEMENTO JSX DEBEMOS UTILIZAR UN DIV O UN FRAGMENT
 
+PARA INTERPOLAR CODIGO DE JS DENTRO DEL CODIGO DE JSX DEBO UTILIZAR LAS LLAVEZ {}
+*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const styles = {
+  p: {
+    color: "blue",
+    fontSize: 10
+  }
+};
 
 function App() {
+
+  let name = "Ivan";
+  let city = "Tapiales";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Fragment>
+     <NavBar/>
+       <div className="App">
+          <h1 style={{color:"red", backgroundColor: "yellow"}}>
+          Bienvenidos a la clase de JSX!
+          </h1>
+          <h2>
+            Mi nombre es {name} y soy {city}
+          </h2>
+          <p style={styles.p}>Mi number is {38/2}</p>
+       </div>
+
+       <div className="container">
+          <h2>No entiendo mucho por el momento</h2>
+       </div>
+   </Fragment>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,35 @@
+import React, { Fragment } from "react";
 import './NavBar.css';
+import logo from './logo512.png';
+
 const NavBar = () => {
 
     return(
-        <nav className="container">
-            <ul>
-                <li className="bottom">Enlace</li>
-                <li className="bottom">Enlace</li>
-                <li className="bottom">Enlace</li>
-                <li className="bottom">Enlace</li>
-            </ul>
-        </nav>
+        <Fragment>
+            <nav className="container navbar">
+                <img src={logo}/>
+                
+                <div className="barraBusqueda">
+                    <input
+                        type="text"
+                        placeholder="Buscar"
+                        className="textField"
+                        name="busqueda">
+                    </input>
+                    <input type="submit" value="Buscar"></input>
+                </div>
+
+                <ul>
+                    <li><a href="https://www.google.com">Inicio</a></li>
+
+                    <li><a href="https://www.google.com">Producto</a></li>
+
+                    <li><a href="https://www.google.com">Nosotros</a></li>
+
+                    <li><a href="https://www.google.com">Preguntas frecuentes</a></li>
+                </ul>
+            </nav>
+        </Fragment>
     )
 };
 
