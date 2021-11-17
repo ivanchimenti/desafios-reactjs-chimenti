65d235a81b54c2e150c7e22933638075ddb12402
ivanchimenti <ivanchimenti@gmail.com>
2021-11-16 23:53:33 -0300

    Desafio Clase 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+import NavBar from './components/NavBar';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function App() {
   return (
     <div className="App">
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css';
+const NavBar = () => {
+
+    return(
+        <nav className="container">
+            <ul>
+                <li className="bottom">Enlace</li>
+                <li className="bottom">Enlace</li>
+                <li className="bottom">Enlace</li>
+                <li className="bottom">Enlace</li>
+            </ul>
+        </nav>
+    )
+};
+
+export default NavBar;
